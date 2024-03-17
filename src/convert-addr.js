2d9770bac9053016e53565261fa746da90e9404e
Dan Connolly <dckc@madmode.com>
2024-03-16 19:07:14 -0500

    feat: derive REV addr from ETH addr

--- src/convert-addr.js
+++ src/convert-addr.js
@@ -1,13 +1,20 @@
 // @ts-check
 import process from 'node:process';
+import crypto from 'node:crypto';
+import { bech32 } from 'bech32';
+import blake from 'blakejs';
 import { Mnemonic, ethers } from 'ethers';
 import { DirectSecp256k1HdWallet } from '@cosmjs/proto-signing';
 import { stringToPath } from '@cosmjs/crypto';
 
 // import { pubkeyToAddress } from '@cosmjs/amino';
 
-import crypto from 'node:crypto';
-import { bech32 } from 'bech32';
+const { freeze } = Object;
+
+const agoricChain = {
+  bech32PrefixAccAddr: 'agoric',
+  coinType: 564,
+};
 
 /** @param {string} msg */
 export const personalDigest = msg => {
@@ -27,7 +34,7 @@ export const recoverPublicKey = (msg, sig) => {
 
 // bech32 spec https://github.com/bitcoin/bips/blob/master/bip-0173.mediawiki 2017-03-20
 
-const pkToBech32 = (data, prefix) => {
+export const pkToBech32 = (data, prefix) => {
   const sha256Digest = crypto
     .createHash('sha256')
     .update(data, 'hex')
@@ -38,21 +45,76 @@ const pkToBech32 = (data, prefix) => {
     .update(sha256Digest, 'hex')
     .digest('hex');
 
-  const bech32Words = bech32.toWords(Buffer.from(ripemd160Digest, 'hex'));
+  const bech32Words = bech32.toWords(Base16.decode(ripemd160Digest));
   const words = new Uint8Array([0, ...bech32Words]);
   const address = bech32.encode(prefix, words);
   return address;
 };
 
-const agoricChain = {
-  bech32PrefixAccAddr: 'agoric',
-  coinType: 564,
-};
-
+// https://github.com/Agoric/agoric-sdk/discussions/5830
 const getAgoricHdPath = (coinType = agoricChain.coinType, account = 0) =>
   stringToPath(`m/44'/${coinType}'/${account}'/0/0`);
 
-const main = async (io = {}) => {
+export const Base16 = freeze({
+  /**
+   * Encode bytes to base 16 string.
+   *
+   * @param {Uint8Array | number[]} bytes
+   * @returns { string }
+   */
+  encode(bytes) {
+    return (
+      Array.from(bytes)
+        // eslint-disable-next-line no-bitwise
+        .map(x => (x & 0xff).toString(16).padStart(2, '0'))
+        .join('')
+    );
+  },
+
+  /**
+   * Decode base 16 string to bytes.
+   *
+   * @param {string} hexStr
+   */
+  decode: hexStr => {
+    const removed0x = hexStr.replace(/^0x/, '');
+    const byte2hex = ([arr, bhi], x) =>
+      bhi ? [[...arr, parseInt(`${bhi}${x}`, 16)]] : [arr, x];
+    const [resArr] = Array.from(removed0x).reduce(byte2hex, [[]]);
+    return Uint8Array.from(resArr);
+  },
+});
+
+// Prefix as defined in https://github.com/rchain/rchain/blob/c6721a6/rholang/src/main/scala/coop/rchain/rholang/interpreter/util/RevAddress.scala#L13
+const REV = { coinId: '000000', version: '00' };
+
+/**
+ * Get REV address from ETH address.
+ *
+ * @param {string} ethAddrRaw
+ * @returns {string | null}
+ *
+ * ref https://github.com/rchain-community/rchain-api/blob/master/src/rev-address.js
+ */
+export const getAddrFromEth = ethAddrRaw => {
+  const { keccak256, encodeBase58, toUtf8Bytes } = ethers;
+  const ethAddr = ethAddrRaw.replace(/^0x/, '');
+  if (!ethAddr || ethAddr.length !== 40) return null;
+
+  // Hash ETH address
+  const ethAddrBytes = Base16.decode(ethAddr);
+  const ethHash = keccak256(ethAddrBytes);
+
+  // Add prefix with hash and calculate checksum (blake2b-256 hash)
+  const payload = `${REV.coinId}${REV.version}${ethHash}`;
+  const payloadBytes = Base16.decode(payload);
+  const checksum = blake.blake2bHex(payloadBytes, undefined, 32).slice(0, 8);
+
+  // Return REV address
+  return encodeBase58(Base16.decode(`${payload}${checksum}`));
+};
+
+export const main = async (io = {}) => {
   const { env = process.env } = io;
   const mnemonic = Mnemonic.fromPhrase(env.REREV_MNEMONIC);
   const wallet = ethers.HDNodeWallet.fromMnemonic(mnemonic);
--- test/convert-addr.test.js
+++ test/convert-addr.test.js
@@ -2,13 +2,18 @@
 
 import anyTest from 'ava';
 import { Mnemonic, ethers } from 'ethers';
-import { personalDigest, recoverPublicKey } from '../src/convert-addr';
+import {
+  getAddrFromEth,
+  personalDigest,
+  recoverPublicKey,
+} from '../src/convert-addr.js';
 
 const acct1 = {
   phrase: `purchase practice song follow wall soda
     matter cloud pudding combine material
     repeat pelican typical renew hold banana
     pigeon machine enact paddle parade hour album`,
+  revAddr: '11111he8e2YukkSLDUtzuZJhfVpENpZAX9RfBZj9Qrw377u6baFB7p',
 };
 
 /** @type {import('ava').TestFn<Awaited<ReturnType<typeof makeTestContext>>>} */
@@ -43,4 +48,12 @@ test('reREV signIn yields public key', async t => {
     publicKey,
   });
   t.is(publicKey, ethWallet.publicKey);
+});
+
+test('derive REV address from ETH address', async t => {
+  const { ethWallet } = t.context;
+  const ethAddr = ethWallet.address;
+  const revAddr = getAddrFromEth(ethAddr);
+  t.log({ ethAddr, revAddr });
+  t.is(revAddr, acct1.revAddr);
 });
